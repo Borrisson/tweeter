733d7b64a4c88c3aad71f0f6178527be49ce87c7
Borrisson <lucborris@gmail.com>
2021-01-26 19:33:37 +0000

    Add momentJS file

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,13 +3,10 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-const moment = require('moment');
-
-
 $("document").ready(function () {
 	function createTweetElement({ user, content, created_at }) {
-    const postTime = moment.unix(created_at).fromNow();
-		return `<article>
+    // const postTime = moment.unix(created_at).fromNow();
+		return `<article class="old-tweets">
     <header class="profile-container">
       <div class="individual-profile">
         <img src="${user.avatars}" alt="" />
@@ -21,7 +18,7 @@ $("document").ready(function () {
      ${content.text}
     </p>
     <footer class="profile-container">
-      <p class="date-posted">${postTime}</p>
+      <p class="date-posted">${created_at}</p>
       <div class="profile-container div">
         <a href="">a</a>
         <a href="">b</a>
@@ -30,5 +27,23 @@ $("document").ready(function () {
     </footer>
   </article>`;
 	}
-});
 
+
+const tweetData = {
+	user: {
+		name: "Newton",
+		avatars: "https://i.imgur.com/73hZDYK.png",
+		handle: "@SirIsaac",
+	},
+	content: {
+		text: "If I have seen further it is by standing on the shoulders of giants",
+	},
+	created_at: 1461116232227,
+};
+
+const $tweet = createTweetElement(tweetData);
+
+// Test / driver code (temporary)
+console.log($tweet); // to see what it looks like
+$("#tweets-container").append($tweet); // to add it to the page so
+});
