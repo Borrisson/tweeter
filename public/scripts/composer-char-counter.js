511b2624d0867f5b3e8d53756585356b7a381573
Borrisson <lucborris@gmail.com>
2021-01-26 12:11:24 +0000

    Fix Lint Style

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,5 +2,8 @@ $("textarea").on("input", function () {
 	const limit = 140;
 	const textLength = $(this).val().length;
 	const value = limit - textLength;
+	value < 0
+		? $("output").css("color", "red")
+		: $("output").css("color", "inherit");
 	$("output").text(`${value}`);
 });
