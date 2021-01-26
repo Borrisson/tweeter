d0e2ad7d06bba3b2588832870557a02e2a095d0c
Borrisson <lucborris@gmail.com>
2021-01-26 18:44:03 +0000

    Add jquery doc.ready

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,9 +1,11 @@
-$("textarea").on("input", function () {
-	const limit = 140;
-	const textLength = $(this).val().length;
-	const value = limit - textLength;
-	value < 0
-		? $("output").css("color", "red")
-		: $("output").css("color", "inherit");
-	$("output").text(`${value}`);
+$(document).ready(function () {
+	$("textarea").on("input", function () {
+		const limit = 140;
+		const textLength = $(this).val().length;
+		const value = limit - textLength;
+		value < 0
+			? $("output").css("color", "red")
+			: $("output").css("color", "inherit");
+		$("output").text(`${value}`);
+	});
 });
