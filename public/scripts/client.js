d663e20e3d9e58d8bbb1ba7609e781d40f932b21
Borrisson <lucborris@gmail.com>
2021-01-26 20:00:17 +0000

    Add momentJS date from now

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,7 +5,7 @@
  */
 $("document").ready(function () {
 	function createTweetElement({ user, content, created_at }) {
-    // const postTime = moment.unix(created_at).fromNow();
+    const postTime = moment.unix(created_at).fromNow();
 		return `<article class="old-tweets">
     <header class="profile-container">
       <div class="individual-profile">
@@ -18,7 +18,7 @@ $("document").ready(function () {
      ${content.text}
     </p>
     <footer class="profile-container">
-      <p class="date-posted">${created_at}</p>
+      <p class="date-posted">${postTime}</p>
       <div class="profile-container div">
         <a href="">a</a>
         <a href="">b</a>
@@ -27,23 +27,4 @@ $("document").ready(function () {
     </footer>
   </article>`;
 	}
-
-
-const tweetData = {
-	user: {
-		name: "Newton",
-		avatars: "https://i.imgur.com/73hZDYK.png",
-		handle: "@SirIsaac",
-	},
-	content: {
-		text: "If I have seen further it is by standing on the shoulders of giants",
-	},
-	created_at: 1461116232227,
-};
-
-const $tweet = createTweetElement(tweetData);
-
-// Test / driver code (temporary)
-console.log($tweet); // to see what it looks like
-$("#tweets-container").append($tweet); // to add it to the page so
 });
