2fe5692051bf2025ae588a2b671ef904466a4ede
Borrisson <lucborris@gmail.com>
2021-01-26 22:57:31 +0000

    Fix duplication/post of elements

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -30,9 +30,16 @@ $("document").ready(function () {
 	};
 
 	const renderTweets = function (tweetDB) {
-		for (let tweetObj of tweetDB) {
-			const parsedTweet = createTweetElement(tweetObj);
-			$("#tweets-container").prepend(parsedTweet);
+		let parsedTweet;
+
+		if (tweetDB.length - $(".old-tweets").length === 1) {
+			parsedTweet = tweetDB.pop();
+			$("#tweets-container").prepend(createTweetElement(parsedTweet));
+		} else {
+			for (let tweetObj of tweetDB) {
+				parsedTweet = createTweetElement(tweetObj);
+				$("#tweets-container").prepend(parsedTweet);
+			}
 		}
 	};
 
@@ -41,10 +48,10 @@ $("document").ready(function () {
 		let text = $("textarea");
 		if (text.val() && text.val().length <= 140) {
 			const data = $(this).serialize();
-      text.val("");
+			text.val("");
 			$.ajax("/tweets", { method: "POST", data: data }).then(loadTweets);
 		} else {
-      console.log('no luck');
+			alert("The message must be between 1 and 140 characters");
 		}
 	});
 
