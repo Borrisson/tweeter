3d6648acc6bd6005c96deb112e99a7fd750575ea
Borrisson <lucborris@gmail.com>
2021-01-26 19:14:49 +0000

    Add createTweetElement function, parses tweetObj to html form

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,4 +3,32 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
+const moment = require('moment');
+
+
+$("document").ready(function () {
+	function createTweetElement({ user, content, created_at }) {
+    const postTime = moment.unix(created_at).fromNow();
+		return `<article>
+    <header class="profile-container">
+      <div class="individual-profile">
+        <img src="${user.avatars}" alt="" />
+        <p>${user.name}</p>
+      </div>
+      <p class="tag-handle">${user.handle}</p>
+    </header>
+    <p class="tweet-message">
+     ${content.text}
+    </p>
+    <footer class="profile-container">
+      <p class="date-posted">${postTime}</p>
+      <div class="profile-container div">
+        <a href="">a</a>
+        <a href="">b</a>
+        <a href="">c</a>
+      </div>
+    </footer>
+  </article>`;
+	}
+});
 
