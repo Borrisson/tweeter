ba0806a84a69bffd55d15363c8db6a85a9d43231
Borrisson <lucborris@gmail.com>
2021-01-26 21:33:52 +0000

    Add AJAX POST on form submit

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,34 +3,6 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
-
-
-
-
 $("document").ready(function () {
 	const createTweetElement = function ({ user, content, created_at }) {
 		const postTime = moment.unix(created_at).fromNow();
@@ -64,5 +36,13 @@ $("document").ready(function () {
 		}
 	};
 
-	renderTweets(data);
+	$("form").on("submit", function (e) {
+		e.preventDefault();
+		const data = $(this).serialize();
+		$("textarea").val("");
+		$.ajax("/tweets", { method: "POST", data: data }).then(function (tweet) {
+			console.log(success);
+		});
+	});
+
 });
